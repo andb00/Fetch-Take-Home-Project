0b385a78ea1ee528979b6824eb32efcb9c386def
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-06-13 13:03:21 -0700

    Initial Commit

---
+++ Fetch Take Home Project/Fetch_Take_Home_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_Home_ProjectApp.swift
+//  Fetch Take Home Project
+//
+//  Created by Andrew Betancourt on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Take_Home_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fetch Take Home ProjectTests/Fetch_Take_Home_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_Home_ProjectTests.swift
+//  Fetch Take Home ProjectTests
+//
+//  Created by Andrew Betancourt on 6/13/25.
+//
+
+import Testing
+@testable import Fetch_Take_Home_Project
+
+struct Fetch_Take_Home_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
